1a9a1253036df12d690879e525746121c59564bf
heroin <david.amadeuss90@gmail.com>
2020-09-24 13:19:25 -0500

    docs agregado

---
+++ docs/precache-manifest.46f40a99f24ba0431bcbd627118a299f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9cb5b9b4e0681e6752bb8f41505b618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a5c3f5fe5afb6395d29",
+    "url": "/static/css/main.0e502cd6.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a5c3f5fe5afb6395d29",
+    "url": "/static/js/main.4490516c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
